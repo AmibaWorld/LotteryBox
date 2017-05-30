127ac74bdc811a6e7b466ef01135aa7a197f54b0
Greatfar <panggreatfar@gmail.com>
2017-05-30 15:30:02 +0800

    优化文件锁

--- Models/LotteryModel.class.php
+++ Models/LotteryModel.class.php
@@ -55,24 +55,27 @@ class LotteryModel{
         );
         // 并发访问，php文件锁,加锁
         $fp = fopen('./Public/file/a.lock', 'r'); // php的文件锁和表没关系，随便一个文件即可
-        flock($fp, LOCK_EX);// 排他锁
-        //查询award表记录总数
-        $sql_total = "select count(*) from lottery_award";
-        $sth = $pdo->prepare($sql_total);
-        $sth->execute() or die("数据库错误: " . $sth->errorInfo()[2]);
-        $arr_total_award = $sth->fetch();                          //匹配成数组
-        $totalNumber = $arr_total_award[0];                        //获取数组第一个元素，即为记录总数
-        if($totalNumber > 300){                                   //抽奖人数超过300人后停止抽奖
-            $totalNumber = 300;
-//            die("you are forbidden");
+        if(flock($fp, LOCK_EX)){    //排他锁
+            //查询award表记录总数
+            $sql_total = "select count(*) from lottery_award";
+            $sth = $pdo->prepare($sql_total);
+            $sth->execute() or die("数据库错误: " . $sth->errorInfo()[2]);
+            $arr_total_award = $sth->fetch();                          //匹配成数组
+            $totalNumber = $arr_total_award[0];                        //获取数组第一个元素，即为记录总数
+            if($totalNumber > 300){                                   //抽奖人数超过300人后停止抽奖
+                $totalNumber = 300;
+            }
+            $award = $arr_award[$totalNumber];
+            //向lottery_award表插入记录
+            $sql_award_ins = "INSERT INTO lottery_award (openid, award) values (?, ?)";
+            $sth = $pdo->prepare($sql_award_ins);
+            $sth->execute(array($openid, $award)) or die("数据库错误: " . $sth->errorInfo()[2]);
+            // php的文件锁，释放锁
+            flock($fp, LOCK_UN);
+        }else{
+            echo "action cancel : locking file error! please try again!";
         }
-        $award = $arr_award[$totalNumber];
-        //更新lottery_award表
-        $sql_award_ins = "INSERT INTO lottery_award (openid, award) values (?, ?)";
-        $sth = $pdo->prepare($sql_award_ins);
-        $sth->execute(array($openid, $award)) or die("数据库错误: " . $sth->errorInfo()[2]);
-        // 并发访问，php的文件锁，释放锁
-        flock($fp, LOCK_UN);
+        //关闭文件
         fclose($fp);
         //更新分配奖项标记isaward
         $sql_upd_flag = "update lottery_userinfo set isaward='true' where openid=?";
